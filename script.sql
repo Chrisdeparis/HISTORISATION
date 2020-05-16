cbb525e4b6456d3f49b1f47ae9281c4faf306f22
maescristro <Chrisdeparis@users.noreply.github.com>
2020-05-16 22:50:37 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -45,11 +45,17 @@ from WWANNEXF.pa1xggipf;
 -- Les infos prets
  SELECT * 
  from t4pprtpf; 
- 
- --PRTBAN = f_BANCODE zoned(5:0)
+  --PRTBAN = f_BANCODE zoned(5:0)
+
  -- pacggi = F_IDGARGARANTIEINCLUSE; 
  -- prtcie = F_CIECODE; 
  -- prtdrt = C_DATEREFERENCETARIFAIRE,
+ -- packro clé
+ -- prtkro clé
+ -- prtnum clé
+ -- apenum clé
+ -- prtord clé
+ -- apeord clé
  -- pactau = C_TAUX
 select pacggi, prtcie, prtdrt 
 from WWADHESF.adh1pacpf 
@@ -67,12 +73,14 @@ where pactau is not null;
          
 -- banque retrouver         
 
--- MADKMV     CONDEN      11  0 = P_IDHISTOMOUVEMENT
--- H43KMV     CONDEN      11  0 = X_IDHISTOMOUVEMENT
+
 -- H43GGI     CONDEN       5  0 = F_IDGARGARANTIEINCLUSE 
 -- PRTBAN     CONDEN       5  0 = F_BANCODE
 -- PRTCIE     CONDEN       3  0 = F_CIECODE
 -- PRTDRT     DATE           10 = C_DATEREFERENCETARIFAIRE
+-- MADKMV     CONDEN      11  0 = P_IDHISTOMOUVEMENT clé
+-- H43KMV     CONDEN      11  0 = X_IDHISTOMOUVEMENT clé
+-- condition h43tau = 0
 select prtcie, prtban, h43ggi, prtdrt 
 from WWADHESF.adh1h43pf
 inner join WWADHESF.p0amadpf on h43kmv=madkmv
